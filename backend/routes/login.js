6da090f2ed232049ba6d547c130dddd6caed144e
Fawad Ahmed <146585202+Fawad992005@users.noreply.github.com>
2024-08-24 18:07:49 +0500

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,36 +1,22 @@
-// routes/login.js
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-const cookieParser = require("cookie-parser");
 const router = express.Router();
-
-
 
 router.post("/", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user)
-      return res.status(401).json({ error: "Invalid email or password" });
+    if (!user) return res.status(401).json({ error: "Invalid email or password" });
 
     const isMatch = await user.comparePassword(password);
-    if (!isMatch)
-      return res.status(401).json({ error: "Invalid email or password" });
+    if (!isMatch) return res.status(401).json({ error: "Invalid email or password" });
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.SUPER_KEY, {
       expiresIn: "1h",
     });
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 3600000,
-      secure: true,
-      sameSite:"None",
-      path: "/"// Use true if using HTTPS
-    });
-    res.json({message:"succesful"})
-
+    res.json({ token }); // Return the token in the response body
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
