8fd1d5e3e4d796346ef76ca52f761825b4a9add2
Fawad Ahmed <146585202+Fawad992005@users.noreply.github.com>
2024-08-24 06:44:00 -0700

    Update checkauth.js

--- backend/middlewares/checkauth.js
+++ backend/middlewares/checkauth.js
@@ -1,14 +1,11 @@
+// middlewares/checkAuth.js
 const jwt = require("jsonwebtoken");
 
 const checkAuth = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  const token = req.cookies.token;
+  if (!token) {
     return res.status(401).json({ error: 'No token provided' });
   }
-
-  const token = authHeader.split(' ')[1]; // Extract the token
-
   try {
     const decoded = jwt.verify(token, process.env.SUPER_KEY);
     req.user = decoded; // Attach decoded user info to req object
@@ -18,4 +15,6 @@ const checkAuth = (req, res, next) => {
   }
 };
 
-module.exports = checkAuth;
+  
+  module.exports = checkAuth;
+  
