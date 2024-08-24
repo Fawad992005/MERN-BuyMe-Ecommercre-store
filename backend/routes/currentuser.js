fcc3715c5c3e883a28fa04acd3f4afe9734d0c3e
Fawad992005 <fawaadahmed514@gmail.com>
2024-08-24 04:20:53 -0700

    check

--- backend/routes/currentuser.js
+++ backend/routes/currentuser.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   if (req.user) {
     res.json(req.user);
+    res.status(200).json({message:"User found"})
   } else {
     res.status(401).json({ message: "Not authenticated" });
   }
