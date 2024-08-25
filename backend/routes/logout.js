67607e5526b50a0f1458a75d450c8f39179e7347
Fawad Ahmed <fawaadahmed514@gmail.com>
2024-08-25 15:49:31 +0500

    Fixed

--- backend/routes/logout.js
+++ backend/routes/logout.js
@@ -3,7 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  res.clearCookie("token",{path:"/"});
+  res.clearCookie("token",{
+    httpOnly: true,
+      secure: true,
+      sameSite:"None",
+      path:"/" 
+  });
   res.json({ message: "Logged out successfully" });
 });
 
